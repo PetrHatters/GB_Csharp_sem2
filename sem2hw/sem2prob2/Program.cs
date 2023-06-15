4cbd5d6bfaa00c427fac0a137a91cedd76cb622d
PetrHatters <psy.entry@gmail.com>
2023-06-15 14:40:02 +0400

    three tasks done

---
+++ sem2hw/sem2prob2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/ 
+Console.WriteLine("Введите целое число:");
+int num = int.Parse(Console.ReadLine());
+int thirdDig = (int)Math.Log10(num)-2;
+if (thirdDig < 0 )
+{
+    Console.WriteLine("No such digit");
+}
+else 
+{
+    Console.WriteLine((num % (int)Math.Pow(10, thirdDig + 1) / (int)Math.Pow(10, thirdDig)).ToString());
+}
